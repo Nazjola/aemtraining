aee500f36079cb89f1b54b02a77861f49894cac4
unknown <you@example.com>
2021-06-10 13:03:28 +0200

    Coinmarket component/ client-lib/ beans

---
+++ core/src/main/java/com/moveo/aem/training/core/beans/CoinmarketBean.java
@@ -0,0 +1,62 @@
+
+package com.moveo.aem.training.core.beans;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "status",
+    "data"
+})
+@Generated("jsonschema2pojo")
+public class CoinmarketBean implements Serializable
+{
+
+    @JsonProperty("status")
+    private Status status;
+    @JsonProperty("data")
+    private Data data;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 8419355944185273461L;
+
+    @JsonProperty("status")
+    public Status getStatus() {
+        return status;
+    }
+
+    @JsonProperty("status")
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @JsonProperty("data")
+    public Data getData() {
+        return data;
+    }
+
+    @JsonProperty("data")
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ core/src/main/java/com/moveo/aem/training/core/beans/Quote__1.java
@@ -0,0 +1,49 @@
+
+package com.moveo.aem.training.core.beans;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "USD"
+})
+@Generated("jsonschema2pojo")
+public class Quote__1 implements Serializable
+{
+
+    @JsonProperty("USD")
+    private Usd__1 usd;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 1774430573853231495L;
+
+    @JsonProperty("USD")
+    public Usd__1 getUsd() {
+        return usd;
+    }
+
+    @JsonProperty("USD")
+    public void setUsd(Usd__1 usd) {
+        this.usd = usd;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
